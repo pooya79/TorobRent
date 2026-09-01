5ef8f2e8089d28a6908b998ede1f5d01ad4aa3fe
Pouya Fallah <pooyafallah79@gmail.com>
2026-09-01 11:59:26 +0330

    fix(backend): serve static assets in tests

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -4,11 +4,8 @@ from .base import env
 SECRET_KEY = "test-secret-key"
 DEBUG = False
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-MIDDLEWARE = [
-    middleware
-    for middleware in MIDDLEWARE  # noqa: F405
-    if middleware != "whitenoise.middleware.WhiteNoiseMiddleware"
-]
+STATIC_ROOT = None
+WHITENOISE_USE_FINDERS = True
 if env("TEST_DATABASE_URL", default=""):
     DATABASES = {"default": env.db_url("TEST_DATABASE_URL")}
 else:
