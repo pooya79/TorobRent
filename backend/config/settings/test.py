079e161ca77b90f99e4a94878ed86ca1c1e73109
Pouya Fallah <pooyafallah79@gmail.com>
2026-09-01 12:14:03 +0330

    fix(backend): remove WhiteNoise from test middleware

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -4,8 +4,11 @@ from .base import env
 SECRET_KEY = "test-secret-key"
 DEBUG = False
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-STATIC_ROOT = None
-WHITENOISE_USE_FINDERS = True
+MIDDLEWARE = [
+    middleware
+    for middleware in MIDDLEWARE  # noqa: F405
+    if middleware != "whitenoise.middleware.WhiteNoiseMiddleware"
+]
 if env("TEST_DATABASE_URL", default=""):
     DATABASES = {"default": env.db_url("TEST_DATABASE_URL")}
 else:
